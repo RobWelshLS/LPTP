567a93be06fb5eebfcd075a004fb1bd581d16e71
Rob Welsh <rob.welsh@lakeshore.com>
2023-06-29 17:17:42 -0400

    Update timesheet_prep.py.

--- timesheet_prep.py
+++ timesheet_prep.py
@@ -2,9 +2,20 @@ import csv
 
 lp_export = []
 
-with open('Sample LP export.csv') as time_entries_file:
-    time_entries_dict = csv.DictReader(time_entries_file)
+with open('Sample LP export.csv', encoding='utf-8-sig') as lp_export_file:
+    time_entries_dict = csv.DictReader(lp_export_file)
     for row in time_entries_dict:
         lp_export.append(row)
 
-print(lp_export)
+new_dict = {}
+
+print(lp_export[0])
+
+new_dict['Company'] = lp_export[0].get('Company')
+emp_num = lp_export[0].get('person_reference')
+new_dict['EmployeeNum'] = emp_num.split("-")[0]
+
+print(new_dict)
+
+# for k, v in lp_export[0].items():
+#     print(f"{k}: {v}")
